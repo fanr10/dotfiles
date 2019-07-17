ec1f85037a673bfaa1fb001a5d3d12106e0bcc86
wangzme <wangzme@outlook.com>
2019-07-17 17:15:43 +0800

    update polybar settings

--- config/polybar/scripts/battery-state.sh
+++ config/polybar/scripts/battery-state.sh
@@ -5,9 +5,17 @@
 STATE=`upower -i /org/freedesktop/UPower/devices/battery_BAT0|grep state`
 
 STATE1="pending-charge"
+STATE2="discharging"
+STATE3="full"
+
+
 
 if echo "$STATE"|grep $STATE1 > /dev/null ;then
-        echo "  􀋩 "
+        echo "  􀂲 "
+elif echo "$STATE"|grep $STATE2 > /dev/null ;then
+        echo "  􀂚 "
+elif echo "$STATE"|grep $STATE3 > /dev/null ;then
+        echo "  􀂞 "
 else
         echo ""
 fi
